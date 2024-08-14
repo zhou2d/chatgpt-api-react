2951f93572d95fab74ba4defb7477cf137ac98e8
zhou2d <61268942+zhou2d@users.noreply.github.com>
2024-08-14 18:33:04 +0800

    Update chat.js

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -34,6 +34,7 @@ export default async function handler(req, res) {
 
       res.status(200).json(response.data);
     } catch (error) {
+      console.log("0.error");
       console.log(error);
       res.status(500).json({ message: "Something went wrong" });
     }
